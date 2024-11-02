ee863c748188b01d96eb621ddc3867c5f9d74f22
Your Name <pallavipradhan5578@gmail.com>
2024-11-02 16:37:31 +0530

    Homepage.dart

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:TodoScreen(),debugShowCheckedModeBanner: false,
+      home:HomePage (),debugShowCheckedModeBanner: false,
     );
   }
 }
